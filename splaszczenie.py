02606311871f2c2e060711586fdc7a81c2cb5643
glebapaulina <paulinagleba@o2.pl>
2017-01-11 12:20:55 +0100

    Update splaszczenie.py

--- splaszczenie.py
+++ splaszczenie.py
@@ -1,7 +1,4 @@
 def splaszczenie(tablica):
-    # podpowiedzi:
-    # isinstance([1,2,3], list) == True
-    # isinstance(5, list) == False
     tab = []
     for x in tablica:
         if isinstance(x, list):
